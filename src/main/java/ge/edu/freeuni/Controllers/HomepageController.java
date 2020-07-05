75e4baa7d63f968e085d07874b60a01242d458e4
NaziOtter <oemnadze@gmail.com>
2020-07-05 18:54:14 +0400

    Empty Project

---
+++ src/main/java/ge/edu/freeuni/Controllers/HomepageController.java
@@ -0,0 +1,16 @@
+package ge.edu.freeuni.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomepageController {
+
+    @GetMapping("")
+    public String login(HttpSession ses) {
+        ses.setAttribute("user", null);
+        return "homepage";
+    }
+}
