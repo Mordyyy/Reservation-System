ce65a2aceb89fce6ecdcb5ee15ed14f51da8a039
mishogch <mgach18@freeuni.edu.ge>
2020-07-06 17:44:03 +0400

    12

--- src/main/java/ge/edu/freeuni/Controllers/HomepageController.java
+++ src/main/java/ge/edu/freeuni/Controllers/HomepageController.java
@@ -1,9 +1,17 @@
 package ge.edu.freeuni.Controllers;
 
+import ge.edu.freeuni.DAO.UsersDAO;
+import ge.edu.freeuni.Models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionListener;
 
 @Controller
 public class HomepageController {
@@ -11,6 +19,21 @@ public class HomepageController {
     @GetMapping("")
     public String login(HttpSession ses) {
         ses.setAttribute("user", null);
-        return "homepage";
+        return "login";
+    }
+
+    @PostMapping("")
+    public String Authenticate(@RequestParam String Username,
+                               @RequestParam String Password,
+                                HttpServletRequest req,
+                                HttpServletResponse resp){
+        UsersDAO users = (UsersDAO)req.getServletContext().getAttribute("db");
+        User user = users.getUser(Username);
+        ModelAndView modelAndView = new ModelAndView("");
+        if(user.getPassword().equals(Password)){
+            modelAndView.addObject("error", "Username or Password incorrect.");
+        }else{
+            return "login";
+        }
     }
 }
