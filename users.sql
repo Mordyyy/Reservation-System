b8e1b587f252b46326e2dde4ea51d6a3f4d6c128
NaziOtter <oemnadze@gmail.com>
2020-07-06 16:23:58 +0400

    SQL

---
+++ users.sql
@@ -0,0 +1,11 @@
+create database Reservation;
+
+use Reservation;
+
+create table users (
+	username varchar(60),
+    password varchar(60),
+    mail varchar(60),
+    gender varchar(60),
+    avatar varchar(60)
+);
