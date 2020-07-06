7734101af6958a89441c3d6ba03940c1873a937c
Achi <Mordy@tests-MacBook-Pro.local>
2020-07-06 17:02:59 +0400

    email

--- src/main/java/ge/edu/freeuni/Models/Email.java
+++ src/main/java/ge/edu/freeuni/Models/Email.java
@@ -1,4 +1,31 @@
 package ge.edu.freeuni.Models;
 
-public class Email {
+import java.util.HashMap;
+import java.util.Random;
+
+public class Email{
+    private HashMap<String,Integer> randomCodes;
+    public Email(){
+        randomCodes = new HashMap<>();
+    }
+    public boolean verifyRandomCode(User user, int code){
+        if(!randomCodes.containsKey(user.getUsername())) return false;
+        return randomCodes.get(user.getUsername()) == code;
+    }
+
+    public void sendCode(User user, String message){
+
+    }
+
+    public void sendRandomCode(User user){
+        int randomCode = getRandomInteger();
+        randomCodes.put(user.getUsername(),randomCode);
+        sendCode(user,Integer.toString(randomCode));
+    }
+
+    private int getRandomInteger(){
+        Random rand = new Random();
+        int randInteger = rand.nextInt(1000000) + 100000;
+        return randInteger;
+    }
 }
