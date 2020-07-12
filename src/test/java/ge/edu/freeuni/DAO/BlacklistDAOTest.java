7fd8222a52fd67fd28d7a1cb34cfc1b9e2d661da
Achi <Mordy@tests-MacBook-Pro.local>
2020-07-12 13:17:46 +0400

    tests root

---
+++ src/test/java/ge/edu/freeuni/DAO/BlacklistDAOTest.java
@@ -0,0 +1,16 @@
+package ge.edu.freeuni.DAO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlacklistDAOTest {
+    private final String url = "jdbc:mysql://localhost";
+    private final String username = "root";
+    private final String password = "4546";
+    @Test
+    public void AddUser(){
+        BlacklistDAO bd = new BlacklistDAO(url,username,password);
+        bd.addUser("Achi");
+    }
+}
